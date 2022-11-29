c246636e3de41de2b2be6c7ea8e1d148b91946eb
sanand100 <shraddhaanand1@gmail.com>
2022-11-28 20:48:59 -0800

    design and render the homepage banner

--- src/Components/Homepage_banner.js
+++ src/Components/Homepage_banner.js
@@ -3,8 +3,21 @@ import './Homepage-banner.css';
 
 function Homepage_banner(props) {
 	return (
+		// <img
+		// 	src={require('../Images/homepage-banner.png')}
+		// 	alt=''
+		// 	className='homepage-banner'
+		// />
 		<div className='homepage-banner'>
-			<img src={require('../Images/homepage-banner.png')} alt='' />
+			<div className='homepage-banner-text'>
+				<h5>ART FOR IMPACT:</h5>
+				<h1>Pakistan Flood Relief Art Show</h1>
+				<h5>03.05.23 | COLONY HOUSE ANAHEIM</h5>
+				<div className='btn-div'>
+					<button>ATTEND</button>
+					<button>I'M AN ARTIST</button>
+				</div>
+			</div>
 		</div>
 	);
 }
